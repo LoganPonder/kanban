33e5af92ec7763fa872e30391984146075e4f5af
Toppedyk <Tayloroppedyk@yahoo.com>
2021-06-15 12:12:09 -0600

    new auth

--- kanban.client/src/env.js
+++ kanban.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'lponder7.us.auth0.com'
-export const audience = 'https://kanban.com'
-export const clientId = 'mmZVXkZoxb5mcaZQg7nCLMMOCxZkro8P'
+export const domain = 'taylor-oppedyk.us.auth0.com'
+export const audience = 'buglog.com'
+export const clientId = '6oZkS3dm4Ro6nyIDJnwMtdRYsxTzDMpS'
